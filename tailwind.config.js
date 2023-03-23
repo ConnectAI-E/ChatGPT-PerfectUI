afbd475b8a5d18471f8c0f9daba7129d60a31287
river <laolei@forkway.cn>
2023-03-23 19:22:57 +0800

    feat: adaptation color in dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        error: 'var(--color-error)',
+        'primary': 'var(--color-primary)',
+        'secondary': 'var(--color-secondary)',
+        'error': 'var(--color-error)',
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
       },
       gradientColorStops: {
         primary: 'var(--gradient-primary)',
